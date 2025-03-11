bae579360a011c007cb24cb1c588723ea1115a56
ZhuravlevMax <l9sik3@gmail.com>
2025-03-11 22:35:52 +0300

    Project was created

---
+++ PhoneBook/PhoneBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhoneBook
+//
+//  Created by Максим Журавлев on 11.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
